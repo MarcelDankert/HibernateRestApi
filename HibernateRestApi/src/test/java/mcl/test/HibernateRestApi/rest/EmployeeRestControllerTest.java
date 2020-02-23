7071df59ed8f9edf504c170e1f79ba1c181b04a8
Marcel Dankert <Marcel Dankert@MCL-DESKTOP-HOME>
2020-02-23 17:56:15 +0100

    added jacoco and sonarqube

---
+++ HibernateRestApi/src/test/java/mcl/test/HibernateRestApi/rest/EmployeeRestControllerTest.java
@@ -0,0 +1,14 @@
+package mcl.test.HibernateRestApi.rest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class EmployeeRestControllerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
